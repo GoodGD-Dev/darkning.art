7761a658df1d966b427d6566a3e2ed928cac80a0
goodgd <dionatha.work@gmail.com>
2024-07-30 12:14:47 -0300

    form e contact feitos

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -53,3 +53,43 @@ document.addEventListener('DOMContentLoaded', () => {
     addNavItemClickEvent(navItems[key], key);
     }
 });
+
+
+
+//form
+
+// Função para mostrar ou esconder o campo "Outros" para o tipo de projeto
+function showOtherInput() {
+    var projectType = document.getElementById('project-type');
+    var otherInput = document.getElementById('other-project-type');
+    if (projectType.value === 'Other') {
+        otherInput.style.display = 'block';
+    } else {
+        otherInput.style.display = 'none';
+    }
+}
+
+// Função para mostrar ou esconder o campo "Outros" para as necessidades de licenciamento
+function showOtherLicensingInput() {
+    var licensingNeeds = document.getElementById('licensing-needs');
+    var otherLicensingInput = document.getElementById('other-licensing-needs');
+    if (licensingNeeds.value === 'Other') {
+        otherLicensingInput.style.display = 'block';
+    } else {
+        otherLicensingInput.style.display = 'none';
+    }
+}
+
+// Adiciona validação personalizada para campos obrigatórios
+document.querySelectorAll('input[required], select[required], textarea[required]').forEach((input) => {
+    input.addEventListener('invalid', (event) => {
+        event.preventDefault();
+        var label = input.previousElementSibling.textContent.replace('*', '').trim();
+        input.setCustomValidity(`Please fill out the "${label}" field.`);
+        input.reportValidity();
+    });
+
+    input.addEventListener('input', (event) => {
+        input.setCustomValidity('');
+    });
+});
